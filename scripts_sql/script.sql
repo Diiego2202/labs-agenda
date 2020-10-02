b7e3dad44eed93814b3784c7f8b38129d32a9198
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2020-10-02 18:16:12 -0300

    novas entidades no banco

--- scripts_sql/script.sql
+++ scripts_sql/script.sql
@@ -9,6 +9,8 @@ CREATE TABLE perfil (
   UNIQUE KEY nome_UN (nome)
 );
 
+
+
 INSERT INTO perfil (nome) VALUES ('ADMINISTRADOR'), ('COMUM');
 
 -- DROP TABLE IF EXISTS usuario;
@@ -28,12 +30,58 @@ CREATE TABLE usuario (
 
 INSERT INTO usuario (login, nome, idperfil, senha, token, criacao) VALUES ('ADMIN', 'ADMINISTRADOR', 1, 'peTcC99vkvvLqGQL7mdhGuJZIvL2iMEqvCNvZw3475PJ:JVyo1Pg2HyDyw9aSOd3gNPT30KdEyiUYCjs7RUzSoYGN', NULL, NOW());
 
+
 CREATE TABLE evento (
-  id int NOT NULL AUTO_INCREMENT,
-  nome varchar(50) NOT NULL,
-  inicio datetime NOT NULL,
-  termino datetime NOT NULL,
-  PRIMARY KEY (id),
-  KEY evento_inicio_termino_ix (inicio, termino),
-  KEY evento_termino_ix (termino)
+  id_evento int NOT NULL AUTO_INCREMENT,
+  nome_evento varchar(50) NOT NULL,
+  desc_evento varchar(50) not null,
+  inicio_evento datetime NOT NULL,
+  termino_evento datetime NOT NULL,
+  PRIMARY KEY (id_evento),
+  KEY evento_inicio_termino_ix (inicio_evento, termino_evento),
+  KEY evento_termino_ix (termino_evento)
 );
+
+
+create table sala(
+	id_sala int primary key auto_increment,
+    desc_sala varchar(45),
+    andar_sala int
+);
+
+create table professor(
+	id_prof int primary key auto_increment,
+    nome_prof varchar(45)
+);
+
+create table evento_prof(
+	id_prof int not null, 
+	foreign key(id_prof) references professor(id_prof),
+    id_evento int not null,
+    foreign key(id_evento) references evento(id_evento),
+    primary key (id_prof, id_evento)
+);
+
+
+create table turma(
+	id_turma int primary key auto_increment, 
+    desc_turma varchar(45)
+);
+
+create table evento_turma(
+	id_evento int not null,
+    foreign key(id_evento) references evento(id_evento),
+    id_turma int not null,
+    foreign key(id_turma) references turma(id_turma),
+    primary key (id_evento, id_turma)
+);
+
+
+
+-- insert into evento(nome, inicio, termino) values
+-- ('ev1', '2020-10-02T08:00', '2020-10-02T09:00'),
+-- ('ev2', '2020-10-02T08:00', '2020-10-02T09:00'),
+-- ('ev3', '2020-10-02T10:00', '2020-10-02T09:00');
+
+
+select * from evento;
